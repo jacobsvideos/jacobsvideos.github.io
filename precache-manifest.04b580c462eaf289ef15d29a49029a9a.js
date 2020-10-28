e71a4f0e7ca5236a97cc69c167e29147a07a36f7
jacobflee <jacoblee00000@gmail.com>
2020-10-28 16:03:46 -0700

    created with react and updated default movie

---
+++ precache-manifest.04b580c462eaf289ef15d29a49029a9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c94b5c2a71aa4b70742c6b840840a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2186f35c5c575b60ebb3",
+    "url": "/static/css/main.9c9f33c1.chunk.css"
+  },
+  {
+    "revision": "b23038eabbe8174b96bf",
+    "url": "/static/js/2.d6993840.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.d6993840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2186f35c5c575b60ebb3",
+    "url": "/static/js/main.de2f18dd.chunk.js"
+  },
+  {
+    "revision": "ead7c0245044f60571de",
+    "url": "/static/js/runtime-main.182ae5d2.js"
+  }
+]);
